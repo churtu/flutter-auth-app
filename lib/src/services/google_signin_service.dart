767b1adf35818e285720713026213f42bdcfd00c
Christian Urtubia R <christian.urodriguez@gmail.com>
2023-01-20 18:55:43 -0300

    Fin seccion 29 - Agregado el sevicio de auth google

--- lib/src/services/google_signin_service.dart
+++ lib/src/services/google_signin_service.dart
@@ -1,15 +1,27 @@
 import 'package:google_sign_in/google_sign_in.dart';
-
+import 'package:http/http.dart' as http;
 class GoogleSigninService {
   static final GoogleSignIn _googleSignIn = GoogleSignIn(
     scopes: ['email'],
   );
 
-  static Future<GoogleSignInAccount?> signinWithGoogle()async{
+  static Future signinWithGoogle()async{
     try {
       final account = await _googleSignIn.signIn();
-      // final googleKey = await account?.authentication; // TOKEN
-      return account;
+      final googleKey = await account?.authentication; // TOKEN
+      
+      final url = Uri(
+        scheme: 'http',
+        host: '10.0.2.2',
+        port: 3000,
+        path: '/google'
+      );
+
+      final response = await http.post(url, body: {
+        'token': googleKey!.idToken
+      });
+
+      return response;
     } catch (e) {
       return null;
     }
